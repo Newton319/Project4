eea66d8ad504af4e3c0f8c4e47dd77350c574f49
Newton <n3831147@gmail.com>
2024-11-05 14:48:41 +0100

    added font

--- src/Component/Hero.jsx
+++ src/Component/Hero.jsx
@@ -1,12 +1,12 @@
-import logo from"../assets/money.jpg"
+import logo from"../assets/logo.jpg"
 function Hero() {
     return(
-        <div className="flex justify-between items-center">
-            <div className="border flex flex-col h-60 items-center justify-center">
-  <p className="font-bold text-xl text-center flex-1 w-32">CJ is a place....</p>
- <button className="w-14 bg-orange-400 rounded-md">Click</button>
+        <div className="flex items-center"> 
+            <div className="border flex flex-1 h-60 flex-col text-center items-center p-4">
+  <p className="font-bold  text-xl flex-1 w-auto">CJ is a place where places are placed in different places and of all the places we only have one place....</p>
+ <button className="w-20 bg-orange-400 rounded-md">Click</button>
 </div>
-            <div className="border flex justify-center h-60">
+            <div className="border flex flex-1 justify-center items-center h-60">
                 <img className="w-60 justify-around items-center" src={logo} alt="logo" />
             </div>
 
